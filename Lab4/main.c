7ed1a89329b4403edd59987cb82e20656346fb9c
alan <alan.d.weide@gmail.com>
2016-04-09 17:30:34 -0400

    segfaults >:(

--- Lab4/main.c
+++ Lab4/main.c
@@ -7,9 +7,53 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+//#include <cuda_builtin_vars.h>
+
+void initMatrix(double** F, int dim) {
+	for (int i = 0; i < dim; i++) {
+		for (int j = 0; j < dim; j++) {
+			F[i][j] = 1.0 + ((double)rand() / RAND_MAX);
+		}
+	}
+}
+
+void printMatrix (double** F, int dim) {
+	for (int i = 0; i < dim; i++) {
+		printf("[ ");
+		for (int j = 0; j < dim; j++) {
+			printf(" %lf", F[i][j]);
+		}
+		printf(" ]\n");
+	}
+	
+}
+
+void computeStuff(double** F, int dim) {
+	for (int k = 0; k < 100; k++)  {
+		for (int i = 1; i < dim; i++) {
+			for (int j = 0; j < dim - 1; j++) {
+				F[i][j] = F[i-1][j+1] + F[i][j+1];
+			}
+		}
+	}
+}
 
 int main(int argc, const char * argv[]) {
-	// insert code here...
-	printf("Hello, World!\n");
-    return 0;
+	int dim = 4097;
+	double** F = malloc(dim * dim * sizeof(double));
+	srand(time(NULL));
+	
+//	initMatrix(F, dim);
+	for (int i = 0; i < dim; i++) {
+		for (int j = 0; j < dim; j++) {
+			F[i][j] = 1.0 + ((double)rand() / RAND_MAX);
+		}
+	}
+	
+//	printMatrix(F, dim);
+	
+	computeStuff(F, dim);
 }
+
