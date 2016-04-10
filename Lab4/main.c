71717c448a0e5fa818a2c74bbb5d705fdc2cb540
alan <alan.d.weide@gmail.com>
2016-04-10 18:38:10 -0400

    CUDA working (compile with -D CUDA flag)

--- Lab4/main.c
+++ Lab4/main.c
@@ -11,60 +11,91 @@
 #include <stdint.h>
 #include <time.h>
 #include <cuda_runtime.h>
+#include <cuda.h>
 
 #define DIM 4097
 #define REPS 100
-double F[DIM][DIM];
 
-void initMatrix() {
-//	F = malloc(dim * dim * sizeof(double));
-	
+void initMatrix(double *F) {
 	for (int i = 0; i < DIM; i++) {
 		for (int j = 0; j < DIM; j++) {
-			F[i][j] = 1.0 + ((double)rand() / RAND_MAX);
+			F[DIM * i + j] = 1.0 + ((double)rand() / RAND_MAX);
 		}
 	}
 }
 
-void cudaInit() {
-	size_t memSize = DIM * DIM;
-	cudaMalloc((void**) &F, memSize);
-}
-
-void printMatrix () {
+void printMatrix (double *F) {
 	for (int i = 0; i < DIM; i++) {
 		printf("[ ");
 		for (int j = 0; j < DIM; j++) {
-			printf(" %lf", F[i][j]);
+			printf(" %lf", F[DIM * i + j]);
 		}
 		printf(" ]\n");
 	}
 	
 }
 
-void computeStuff() {
+void computeStuff(double *F) {
 	for (int k = 0; k < REPS; k++)  {
 		for (int i = 1; i < DIM; i++) {
 			for (int j = 0; j < DIM - 1; j++) {
-				F[i][j] = F[i-1][j+1] + F[i][j+1];
+				F[DIM * i + j] = F[DIM * (i-1) + (j+1)] + F[DIM * i + (j+1)];
 			}
 		}
 	}
 }
 
-__global__ void cudaCompute(double** F) {
+__global__ void cudaCompute(double* F) {
+	int bid = blockIdx.x;
+	int tid = threadIdx.x;
+
+	for (int k = 0; k < REPS; k++)  {
+		for (int i = bid + 1; i < DIM; i+=gridDim.x) {
+			for (int j = tid; j < DIM-1; j+=blockDim.x) {
+				F[DIM * i + j] = F[DIM * (i-1) + (j+1)] + F[DIM * i + (j+1)];
+			}
+		}
+	}
 	
+	if (tid == 0) {
+		printf("done with CUDA kernel\n");
+	}
 }
 
 int main(int argc, const char * argv[]) {
 	srand((uint32_t)time(NULL));
-	initMatrix();
-	cudaInit();
-//	printMatrix();
+	size_t memSize = DIM * DIM * sizeof(double);
+	double* F =(double*) malloc(memSize);
+	
+	initMatrix(F);
+	
+#ifdef CUDA
+	double* d_F;
+	cudaMalloc((void**) &d_F, memSize);
+	cudaMemcpy(d_F, F, memSize, cudaMemcpyHostToDevice);
+	int nBlocks = 4;
+	int tpb = 1024;
+	dim3 dimGrid(nBlocks);
+	dim3 dimBlock(tpb);
+#endif
+	
 	clock_t clock_start = clock();
-	computeStuff();
+	
+#ifdef CUDA
+	cudaCompute<<<dimGrid, dimBlock>>>(d_F);
+	
+	printf("past CUDA kernel\n");
+#else
+	computeStuff(F);
+#endif
+	
 	clock_t clock_duration = clock() - clock_start;
-	printf("Computation duration: %.3lfs; Performance: %.3lf GFlops\n",
+	
+#ifdef CUDA
+	cudaMemcpy(F, d_F, memSize, cudaMemcpyDeviceToHost);
+#endif
+	
+	printf("Computation duration: %lfs; Performance: %lf GFlops\n",
 		   clock_duration / 1000000.0,
 		   0.001 * REPS * (DIM * DIM) / clock_duration);
 }
