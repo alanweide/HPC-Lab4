9b4fa943d7761caeb1a1bee342c649ecd2827f40
alan <alan.d.weide@gmail.com>
2016-04-10 16:13:34 -0400

    dim/reps to #defines

--- Lab4/main.c
+++ Lab4/main.c
@@ -10,25 +10,31 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <time.h>
+#include <cuda_runtime.h>
 
-const int dim = 4097;
-const int reps = 100;
-double F[dim][dim];
+#define DIM 4097
+#define REPS 100
+double F[DIM][DIM];
 
 void initMatrix() {
 //	F = malloc(dim * dim * sizeof(double));
 	
-	for (int i = 0; i < dim; i++) {
-		for (int j = 0; j < dim; j++) {
+	for (int i = 0; i < DIM; i++) {
+		for (int j = 0; j < DIM; j++) {
 			F[i][j] = 1.0 + ((double)rand() / RAND_MAX);
 		}
 	}
 }
 
+void cudaInit() {
+	size_t memSize = DIM * DIM;
+	cudaMalloc((void**) &F, memSize);
+}
+
 void printMatrix () {
-	for (int i = 0; i < dim; i++) {
+	for (int i = 0; i < DIM; i++) {
 		printf("[ ");
-		for (int j = 0; j < dim; j++) {
+		for (int j = 0; j < DIM; j++) {
 			printf(" %lf", F[i][j]);
 		}
 		printf(" ]\n");
@@ -37,22 +43,29 @@ void printMatrix () {
 }
 
 void computeStuff() {
-	for (int k = 0; k < reps; k++)  {
-		for (int i = 1; i < dim; i++) {
-			for (int j = 0; j < dim - 1; j++) {
+	for (int k = 0; k < REPS; k++)  {
+		for (int i = 1; i < DIM; i++) {
+			for (int j = 0; j < DIM - 1; j++) {
 				F[i][j] = F[i-1][j+1] + F[i][j+1];
 			}
 		}
 	}
 }
 
+__global__ void cudaCompute(double** F) {
+	
+}
+
 int main(int argc, const char * argv[]) {
 	srand((uint32_t)time(NULL));
 	initMatrix();
+	cudaInit();
 //	printMatrix();
 	clock_t clock_start = clock();
 	computeStuff();
 	clock_t clock_duration = clock() - clock_start;
-	printf("Computation duration: %.3lfs; Performance: %.3lf GFlops\n", clock_duration / 1000000.0, 0.001 * reps * (dim * dim) / clock_duration);
+	printf("Computation duration: %.3lfs; Performance: %.3lf GFlops\n",
+		   clock_duration / 1000000.0,
+		   0.001 * REPS * (DIM * DIM) / clock_duration);
 }
 
